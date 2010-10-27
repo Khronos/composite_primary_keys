e70b2d3cf5ca0fb48aa65c31d54ab9bbd5d625ac
Martin Little <mlittle@tempusfactor.com>
2010-10-27 10:56:21 -0400

    Reved version

--- lib/composite_primary_keys/version.rb
+++ lib/composite_primary_keys/version.rb
@@ -2,7 +2,7 @@ module CompositePrimaryKeys
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 0
+    TINY  = 1
     PATCH = 0
     STRING = [MAJOR, MINOR, TINY, PATCH].join('.')
   end
